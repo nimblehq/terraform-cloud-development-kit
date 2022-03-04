94b0c891fb4655fb888a4f06ca7ff4afd7d7fac2
Junan Chakma <junan@nimblehq.co>
2022-03-04 13:56:25 +0700

    [1] Initialize Go CDK

---
+++ go/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+	"github.com/hashicorp/terraform-cdk-go/cdktf"
+)
+
+func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
+	stack := cdktf.NewTerraformStack(scope, &id)
+
+	// The code that defines your stack goes here
+
+	return stack
+}
+
+func main() {
+	app := cdktf.NewApp(nil)
+
+	stack := NewMyStack(app, "{{ $base }}")
+	cdktf.NewRemoteBackend(stack, &cdktf.RemoteBackendProps{
+		Hostname:     jsii.String("app.terraform.io"),
+		Organization: jsii.String("junan"),
+		Workspaces:   cdktf.NewNamedRemoteWorkspace(jsii.String("Go-Deploy-Namespace")),
+	})
+
+	app.Synth()
+}
