439c2c4527510315d99af2a12d706120f88a86b8
Junan Chakma <junan@nimblehq.co>
2022-03-12 15:21:34 +0700

    [1] Add resources

--- go/main.go
+++ go/main.go
@@ -4,20 +4,33 @@ import (
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
+
+	"cdk.tf/go/stack/generated/hashicorp/aws"
+	"cdk.tf/go/stack/generated/hashicorp/aws/ec2"
 )
 
 func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
 	stack := cdktf.NewTerraformStack(scope, &id)
 
-	// The code that defines your stack goes here
+	aws.NewAwsProvider(stack, jsii.String("AWS"), &aws.AwsProviderConfig{
+		Region: jsii.String("us-west-1"),
+	})
+
+	instance := ec2.NewInstance(stack, jsii.String("compute"), &ec2.InstanceConfig{
+		Ami:          jsii.String("ami-01456a894f71116f2"),
+		InstanceType: jsii.String("t2.micro"),
+	})
+
+	cdktf.NewTerraformOutput(stack, jsii.String("public_ip"), &cdktf.TerraformOutputConfig{
+		Value: instance.PublicIp(),
+	})
 
 	return stack
 }
 
 func main() {
 	app := cdktf.NewApp(nil)
-
-	stack := NewMyStack(app, "{{ $base }}")
+	stack := NewMyStack(app, "aws_instance")
 	cdktf.NewRemoteBackend(stack, &cdktf.RemoteBackendProps{
 		Hostname:     jsii.String("app.terraform.io"),
 		Organization: jsii.String("nimble"),
