9096af3a3dd2b7475d8cc473ed49ac1a494ca2a7
Long Nguyen <nguyenduclong@msn.com>
2022-03-20 22:17:39 +0700

    [#3] Use Terraform Cloud as remote backend

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,14 +1,20 @@
 import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
+import { App, RemoteBackend, TerraformStack } from "cdktf";
 
 class CentauriStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    // define resources here
+    new RemoteBackend(this, {
+      hostname: "app.terraform.io",
+      organization: "nimble",
+      workspaces: {
+        name: "nimble-growth-37-centauri-web-typescript",
+      },
+    });
   }
 }
 
 const app = new App();
-new CentauriStack(app, "tmp");
+new CentauriStack(app, "staging");
 app.synth();
