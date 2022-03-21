3234de0037b81279496731170d8df41ec3d58e1c
Long Nguyen <nguyenduclong@msn.com>
2022-03-21 08:55:31 +0700

    [#3] Define RDS

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,20 +1,40 @@
-import { Construct } from "constructs";
-import { App, RemoteBackend, TerraformStack } from "cdktf";
+import { Construct } from 'constructs';
+import { App, RemoteBackend, TerraformStack } from 'cdktf';
+import { AwsProvider } from '@cdktf/provider-aws'
+import { RandomProvider } from '@cdktf/provider-random'
+
+import { Rds } from './construct'
 
 class CentauriStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
     new RemoteBackend(this, {
-      hostname: "app.terraform.io",
-      organization: "nimble",
+      hostname: 'app.terraform.io',
+      organization: 'nimble',
       workspaces: {
-        name: "nimble-growth-37-centauri-web-typescript",
+        name: 'nimble-growth-37-centauri-web-typescript',
       },
     });
+
+    new RandomProvider(this, "random", {});
+
+    new AwsProvider(this, 'AWS', {
+      region: 'ap-southeast-1'
+    })
+
+    new Rds(this, 'staging', {
+      identifier: 'centauri-typescript',
+      engine: 'postgres',
+      engineVersion: '14.2',
+      instanceClass: 'db.t3.small',
+      allocateStorage: 5,
+      name: 'centauritypescript',
+      username: 'postgres'
+    })
   }
 }
 
 const app = new App();
-new CentauriStack(app, "staging");
+new CentauriStack(app, "centauri-typescript");
 app.synth();
